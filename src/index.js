fa0764c62d5fd97573d275fe84fd67558644abd7
Server <server@server.com>
2020-09-29 10:41:45 +0300

    docs: src/index.js file added own codes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined || array.length == 0){
+        return 0;
+    }else{
+        array.sort((a,b) => a-b);
+       return array[0];
+    }
+
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length == 0){
+        return 0;
+    }else{
+        array.sort((a,b) => a-b);
+       return array[array.length-1];
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length == 0){
+        return 0;
+    }else{
+    let sum = 0;
+    let result
+       for (let i = 0; i < array.length; i++){
+       		sum = sum + array[i];
+          result = sum / array.length;
+       }
+       return result ;
+    }
 }
